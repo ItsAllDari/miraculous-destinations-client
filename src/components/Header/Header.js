7c7c8128a24eb3a8d912bb29bed51a0d416615e6
ItsAllDari <itsalldari@gmail.com>
2020-08-10 11:08:23 -0400

    Updated links and names for the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,7 +23,7 @@ const alwaysOptions = (
 )
 
 const Header = ({ user }) => (
-  <Navbar bg="primary" variant="dark" expand="md">
+  <Navbar bg="success" variant="dark" expand="md">
     <Navbar.Brand href="#">
       Miraculous Destinations
     </Navbar.Brand>
