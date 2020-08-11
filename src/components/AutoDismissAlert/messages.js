51351982e5070633d8016d89765b13f3eedeabac
ItsAllDari <itsalldari@gmail.com>
2020-08-11 03:49:13 -0400

    Added new success and failure messages

--- src/components/AutoDismissAlert/messages.js
+++ src/components/AutoDismissAlert/messages.js
@@ -5,5 +5,13 @@ export default {
   signInFailure: 'Failed to sign in. Check your email and password and try again.',
   signOutSuccess: 'Come back soon!',
   changePasswordSuccess: 'Password changed successfully!',
-  changePasswordFailure: 'Failed to change passwords. Check your old password and try again.'
+  changePasswordFailure: 'Failed to change passwords. Check your old password and try again.',
+  createLocationSuccess: 'New location added!',
+  createLocationFailure: 'Failed to add new location.',
+  showLocationsSuccess: 'Showing all of your locations.',
+  showLocationsFailure: 'Failed to show your locations.',
+  showLocationSuccess: 'Showing this city\'s information',
+  showLocationFailure: 'Could not show city information',
+  editLocationSuccess: 'Successfully updated city information',
+  editLocationFailure: 'Could not properly update city information'
 }
